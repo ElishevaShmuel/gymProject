5daaea2191246e7058fb03083924e979400eef37
ElishevaShmuel <elishevas2004@gmail.com>
2024-12-02 18:58:54 +0200

    my commit message

---
+++ gym.data/Repositories/teacherRepository.cs
@@ -0,0 +1,19 @@
+﻿using gym.core.@interface;
+using gym.core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gym.data.Repositories
+{
+    public class teacherRepository: IteacherRepository
+    {
+        private readonly IDataContext _context;
+        public teacherRepository(IDataContext context)
+        {
+            _context = context;
+        }
+    }
+}
